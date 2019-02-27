07bdfc2c728c09ebd4a1ec6d5752f4f03238fe74
Sherief El-Feky <sherief@b-c-studio.com>
2019-02-27 23:09:21 +0200

    fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="/images/favicon.png" type="image/png">
   <title>@yield('title') | The TriFactory</title>
   <meta name="description" content="The home of triathlon in Egypt. All the information about upcoming events, trips and training services.">
   <!-- CSS -->
