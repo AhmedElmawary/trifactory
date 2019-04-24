02dc2575f7cc1d0e7939a3baad71dd9b6a7e3f5a
Sherief El-Feky <sherief@b-c-studio.com>
2019-04-25 01:08:42 +0200

    verify text changes

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -4,17 +4,17 @@
 <div class="container">
     <section class="login-section no-height">
        <div class="row">
-            <div class="col-lg-6 offset-lg-3 text-center">
+            <div class="col-lg-7 offset-lg-3 text-center">
                 <h3>{{ __('Verify Your Email Address') }}</h3>
                     <div>
                     @if (session('resent'))
                         <div class="alert alert-info" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('A new verification link has been sent to your email address.') }}
                         </div>
                     @endif
 
                     <h6>{{ __('Before proceeding, please check your email for a verification link.') }}</h6>
-                    {{ __('If you did not receive the email') }}, <a class="dark mt-2" href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('If you did not receive the email') }}, <a class="dark mt-2" href="{{ route('verification.resend') }}">{{ __('click here to resend the verification email') }}</a>.
                 </div>
             </div>
         </div>
