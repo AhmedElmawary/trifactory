f2d6a4bb4ad9f531da36e7960f2dda8ed8edf1b4
GenedyMohamed <genedymohamed96@gmail.com>
2019-07-29 18:10:29 +0200

    adding version for main.js script to update in cache

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -207,7 +207,7 @@
         <script src="/js/bootstrap.min.js"></script>
         <script src="/js/slick.min.js"></script>
         <!-- <script src="/js/app.js"></script>-->
-        <script src="/js/main.js"></script>
+        <script src="/js/main.js?v1.1"></script>
 
         <!-- End JS -->
     </body>
