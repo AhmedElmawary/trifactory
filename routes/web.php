d47ed105ae1a6bb112568d406a66b80cb3ea19c7
GenedyMohamed <genedymohamed96@gmail.com>
2019-08-19 17:11:47 +0200

    fixed email verify and added user race count metric backend

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', 'HomeController@index');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
 
 Route::get('/events', 'EventsController@index');
 
