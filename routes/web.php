ca631dc3306f96c7a2a1172125b33bf1fb5823d7
GenedyMohamed <genedymohamed96@gmail.com>
2019-08-28 16:26:04 +0200

    clearing cache beta

--- routes/web.php
+++ routes/web.php
@@ -56,15 +56,3 @@ Auth::routes(['verify' => true]);
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/test', 'HomeController@test');
-
-Route::get('/clear-cache', function() {
-    $exitCode = Artisan::call('config:clear');
-    $exitCode = Artisan::call('cache:clear');
-    $exitCode = Artisan::call('config:cache');
-    return 'DONE'; //Return anything
-});
-
-Route::get('/backup-run', function() {
-    $exitCode = Artisan::call('backup:run');
-    return 'DONE'; //Return anything
-});
