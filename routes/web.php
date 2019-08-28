4947dd630fe95987ad7ab91f27182baa9a9ef279
GenedyMohamed <genedymohamed96@gmail.com>
2019-08-28 13:33:27 +0200

    adding /clear-cache url

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,10 @@ Auth::routes(['verify' => true]);
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/test', 'HomeController@test');
+
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    return 'DONE'; //Return anything
+});
