207a7e6823bb5f98d263abbec7fda62c3fc80163
Mohamed Genedy <genedymohamed96@gmail.com>
2019-09-02 01:58:53 +0200

    backup working version on ubuntu local

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,3 @@ Auth::routes(['verify' => true]);
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
 Route::get('/test', 'HomeController@test');
-
-Route::get('/test', );
-
