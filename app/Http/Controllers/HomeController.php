f631477995e28b187eba4e28d1e5588f454e1252
mohamed_sakr <mohamed1.sakr1996@gmail.com>
2020-03-09 11:26:37 +0200

    fix style

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,9 @@ class HomeController extends Controller
         $gallery = Gallery::latest('created_at')->with('galleryimage')->first();
         $upcomingEvents = Event::with('eventimages')->upcomming()->published()->get();
         $comingSoonEvents = Event::with('eventimages')->comingsoon()->get();
-        $allEvents = \DB::select("SELECT DISTINCT YEAR(created_at) AS year FROM leaderboard_data ORDER BY YEAR(created_at) DESC");
+        $allEvents = \DB::select(
+            "SELECT DISTINCT YEAR(created_at) AS year FROM leaderboard_data ORDER BY YEAR(created_at) DESC"
+        );
         // var_dump($allEvents);
         $years = [];
         foreach ($allEvents as $event) {
