491cb37e56e2997440427803f5232b8d2dc89a3f
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2020-05-07 09:20:47 +0200

    increase memory  and execution time

--- app/Imports/LeaderboardDataImport.php
+++ app/Imports/LeaderboardDataImport.php
@@ -29,8 +29,8 @@ class LeaderboardDataImport implements
 
     public function __construct()
     {
-        // ini_set('memory_limit', '-1');
-        // ini_set('max_execution_time', 3000);
+        ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', 3000);
         $this->sheetName = null;
     }
 
