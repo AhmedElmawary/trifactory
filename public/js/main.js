773886e2f03d1c682a1a1f4263a443901abbf6d2
ahmed_elmawardy <ahmedelmawrdy2@gmail.com>
2020-05-18 16:01:29 +0200

    edit static string for file input

--- public/js/main.js
+++ public/js/main.js
@@ -818,7 +818,7 @@ $(document).ready(function () {
                                         : "") +
                                     "/>";
                             }
-
+                                    
                             if (question.answertype.type === "file") {
                                 str +=
                                     '<label style="cursor: pointer;" class="form-control" for="national_id_image">';
@@ -840,7 +840,8 @@ $(document).ready(function () {
                                     ' style="opacity: 0;width: 1%;float: left;"';
                                 str += " />";
                                 str +=
-                                    ' <span class="fas fa-upload"></span> &nbsp;&nbsp;Upload valid copy of ID/Passport (Less than 25MB ) <span style="float:right;color:green; display:none;" class="far fa-check-circle"></span><span style="float:right;color:red;" class="far fa-times-circle"></span></label>';
+                                    // ' <span class="fas fa-upload"></span> &nbsp;&nbsp;Upload valid copy of ID/Passport (Less than 25MB ) <span style="float:right;color:green; display:none;" class="far fa-check-circle"></span><span style="float:right;color:red;" class="far fa-times-circle"></span></label>';
+                                    ' <span class="fas fa-upload"></span> &nbsp;&nbsp;' +question.question_text +'<span style="float:right;color:green; display:none;" class="far fa-check-circle"></span><span style="float:right;color:red;" class="far fa-times-circle"></span></label>';
                             }
 
                             if (question.answertype.type === "dropdown") {
