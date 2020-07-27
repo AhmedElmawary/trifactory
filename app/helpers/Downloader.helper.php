868c45f2f513f18660530e642199c9eb1400bca4
ahmed_elmawardy <ahmedelmawrdy2@gmail.com>
2020-07-27 23:49:28 +0200

    fxing file extension

--- app/helpers/Downloader.helper.php
+++ app/helpers/Downloader.helper.php
@@ -7,7 +7,7 @@ class DownloaderHelper
 {
     public static function downloadFileToStorage(String $from, $fileName) :void
     {
-        $fileName .=".jpg";
+        $fileName .=".jpeg";
         $response = file_get_contents($from);
        
         Storage::disk('profile_images')->put($fileName, $response);
