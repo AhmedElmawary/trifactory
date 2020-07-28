4f935d84edf4231d3aa4bb39cc511b97669b7e32
ahmed_elmawardy <ahmedelmawrdy2@gmail.com>
2020-07-28 19:50:17 +0200

    fixing api 500

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -392,8 +392,12 @@ class CartController extends Controller
         if (empty($input)) {
             redirect()->back();
         }
+        
+        if (\Request::is('api*') || \Request::wantsJson()) {
+            return $this->index($request);
+        }
 
-        $number_of_tickets = $input['number_of_tickets'] ?? "";
+        $number_of_tickets = $input['number_of_tickets'] ;
 
 
         $grouppedInput = [];
@@ -567,9 +571,6 @@ class CartController extends Controller
         //         'cartTotal' => $cartTotal,
         //     ]);
         // }
-        if (\Request::is('api*') || \Request::wantsJson()) {
-            return $this->index($request);
-        }
 
         return redirect()->action(
             'CartController@index'
